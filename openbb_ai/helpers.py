ba9939f91b2f797309eb48836506d2211bf21d2f
Michael Struwig <mnicstruwig@gmail.com>
2025-05-27 16:21:57 +0200

    Add helpers for creating and yielding citations

--- openbb_ai/helpers.py
+++ openbb_ai/helpers.py
@@ -1,13 +1,18 @@
 from typing import Any, Literal
 
 from .models import (
+    Citation,
+    CitationCollection,
+    CitationCollectionSSE,
     DataSourceRequest,
     FunctionCallSSE,
     FunctionCallSSEData,
     MessageChunkSSE,
     MessageChunkSSEData,
+    SourceInfo,
     StatusUpdateSSE,
     StatusUpdateSSEData,
+    Widget,
     WidgetRequest,
 )
 
@@ -74,8 +79,8 @@ def get_widget_data(widget_requests: list[WidgetRequest]) -> FunctionCallSSE:
     """Create a function call that retrieve data for a widget on the OpenBB Workspace
 
     The function call is typically `yield`ed to the client. After yielding this
-    event, you must immediately close the connection and wait for the follow-up
-    request from the client.
+    function call, you must immediately close the connection and wait for the
+    follow-up function call.
 
     Parameters
     ----------
@@ -107,3 +112,61 @@ def get_widget_data(widget_requests: list[WidgetRequest]) -> FunctionCallSSE:
             input_arguments={"data_sources": data_sources},
         )
     )
+
+
+def cite(
+    widget: Widget,
+    input_arguments: dict[str, Any],
+    extra_details: dict[str, Any] | None = None,
+) -> Citation:
+    """Create a citation for a widget.
+
+    Parameters
+    ----------
+    widget: Widget
+        The widget to cite. Typically retrieved from the `QueryRequest` object.
+    input_arguments: dict[str, Any] | None
+        The input arguments used to retrieve data from the widget.
+    extra_details: dict[str, Any] | None
+        Extra details to display in the citation.
+        Takes key-value pairs of the form `{"key": "value"}`.
+        Default is None.
+
+    Returns
+    -------
+    Citation
+        The citation.
+        Typically used as input to the `citations` function to be returned to
+        the client.
+    """
+    return Citation(
+        source_info=SourceInfo(
+            type="widget",
+            origin=widget.origin,
+            widget_id=widget.widget_id,
+            metadata={
+                "input_args": input_arguments,
+            },
+        ),
+        details=[extra_details] if extra_details else None,
+    )
+
+
+def citations(citations: list[Citation]) -> CitationCollectionSSE:
+    """Create a citation collection SSE.
+
+    This SSE is used to stream back citations to the client.
+
+    This Server-Sent Event (SSE) is typically `yield`ed to the client.
+
+    Parameters
+    ----------
+    citations: list[Citation]
+        The citations to display.
+
+    Returns
+    -------
+    CitationCollectionSSE
+        The citation collection SSE.
+    """
+    return CitationCollectionSSE(data=CitationCollection(citations=citations))
